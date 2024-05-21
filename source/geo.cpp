beb8f684dc4496ba10290ed9556f5bdf0f2ef5dd
1abobik1 <dima15042004@mail.ru>
2024-05-21 11:21:29 +0300

    доделал файл geo

--- source/geo.cpp
+++ source/geo.cpp
@@ -9,6 +9,14 @@ namespace geo {
         return (std::abs(lhs.lat - rhs.lat) < 1e-6 && std::abs(lhs.lng - rhs.lng) < 1e-6);
     }
 
-    
+    double ComputeDistance(Coordinates from, Coordinates to) {
+        using namespace std;
+        const double dr = M_PI / 180.0;
+        const double R = 6371000;
+        double p1 = (from.lat * dr), p2 = (to.lat * dr);
+        double dl = std::abs(from.lng - to.lng);
+        double CosSigma = sin(p1) * sin(p2) + cos(p1) * cos(p2) * cos(dl * dr);
+        return std::acos(CosSigma) * R;
+    }
 
 }  // namespace geo
